3572c42a4adf8580dbe48bf9ec34dae9b8d02fde
yujunjie0603 <yu.long.fr@gmail.com>
2023-06-21 09:49:41 +0200

    ajouter nextauth google connexion et mongodb

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../lib/mongodb"
+
+export const authOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      }),
+    ],
+    adapter: MongoDBAdapter(clientPromise)
+  }
+export default NextAuth(authOptions)
