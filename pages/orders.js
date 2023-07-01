0ea35d39a640ddfbd2449aa8b707c79d8c4cd242
yujunjie0603 <yu.long.fr@gmail.com>
2023-07-01 16:59:07 +0200

    add orders

---
+++ pages/orders.js
@@ -0,0 +1,54 @@
+import Layout from '@/components/Layout'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function ordersPage() {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        axios.get('/api/orders').then(response => {
+            console.log(response.data);
+            setOrders(response.data)
+        })
+    }, []);
+  return (
+    <Layout>
+        <h1>Orders</h1>
+        <table className='basic'>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Paid</td>
+                    <td>Recipient</td>
+                    <td>Products</td>
+                </tr>
+            </thead>
+            <tbody>
+                {orders.length > 0 && orders.map(order => (
+                <tr key={order._id}>
+                    <td>{(new Date(order.createdAt)).toLocaleString()}
+                    </td>
+                    <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
+                    {order.paid ? 'YES' : 'NO'}
+                    </td>
+                    <td>
+                    {order.name} {order.email}<br />
+                    {order.city} {order.postalCode} {order.country}<br />
+                    {order.streetAddress}
+                    </td>
+                    <td>
+                    {order.line_items.map(l => (
+                        <>
+                        {l.price_data?.product_data.name} x
+                        {l.quantity}<br />
+                        </>
+                    ))}
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    </Layout>
+  );
+}
+
+export default ordersPage
