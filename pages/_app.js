76069b70678176727db31ca8fd42e7f43334c459
yujunjie0603 <yu.long.fr@gmail.com>
2023-07-05 19:33:08 +0200

    fini projet

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { SessionProvider } from "next-auth/react"
 import '@/styles/globals.css'
+import '@/styles/stats.css'
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,15 @@
+import HomeHeader from "@/components/HomeHeader";
+import HomeStats from "@/components/HomeStats";
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
-
 
 function Home() {0
     const {data: session} = useSession();
 
     return (
         <Layout > 
-            <div className="text-blue-600 flex justify-between">
-
-                <h2>
-                    hello, {session?.user?.email}
-                </h2>
-                <div className="flex bg-gray-300 gap-1 text-black rounded-md rounded-r">
-                    <img src={session?.user?.image} alt="" className="w-6 h-6"/>
-                    {session?.user?.name}
-                </div> 
-            </div>
+            <HomeHeader />
+            <HomeStats />
         </Layout>
     )
 }
