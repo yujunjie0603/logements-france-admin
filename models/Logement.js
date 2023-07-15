9b4f9418f9a25276e0d27bea8251f382bda8e422
yujunjie0603 <yu.long.fr@gmail.com>
2023-07-15 23:25:07 +0200

    ajouter page logements

---
+++ models/Logement.js
@@ -0,0 +1,26 @@
+import mongoose, {model, Schema, models} from "mongoose";
+
+
+const LogementsSchema = new Schema({
+    
+    name_resident: {type:String, require: true},
+    description: String,
+    type_apartment: {type:String, require: true},
+    price_origin_apartment: {type: String},
+    price_promo_apartment: {type: String},
+    adress: {type:String, require: true},
+    images: [{type:String}],
+    info_apartment: String,
+    tels: [{type:String}],
+    advantages: [{type:String}],
+    services_include: [{type:String}],
+    services_carte: [{type:String}],
+    category: {
+        type:mongoose.Types.ObjectId, ref: 'Category'
+    },
+    properties: {type:Object},
+}, {
+    timestamps: true,
+  });
+
+export const Logement = models.Logement || model('Logement', LogementsSchema);
---
+++ pages/api/logements.js
@@ -0,0 +1,97 @@
+import clientPromise from "@/lib/mongodb";
+import axios from "axios";
+import mongoose from "mongoose";
+import {Logement} from "@/models/Logement"
+import {mongooseConnect} from "@/lib/mongoose"
+import { isAdminRequest } from "./auth/[...nextauth]";
+
+
+export default async function handler(req, res) {
+    const {method} = req;
+    await mongooseConnect();
+    await isAdminRequest(req, res);    
+    if (method === 'GET') {
+        if (req.query?.id) {
+          res.json(await Logement.findOne({_id:req.query.id}));
+        } else {
+          res.json(await Logement.find());
+        }
+      }
+    if (method === 'POST') {
+        const {
+          name_resident,
+          description,
+          type_apartment,
+          price_origin_apartment,
+          price_promo_apartment,
+          adress,
+          images,
+          info_apartment,
+          tels,
+          advantages,
+          services_include,
+          services_carte,
+          category,
+          properties,
+        } = req.body;
+        const logementDoc = await Logement.create({
+          name_resident,
+          description,
+          type_apartment,
+          price_origin_apartment,
+          price_promo_apartment,
+          adress,
+          images,
+          info_apartment,
+          tels,
+          advantages,
+          services_include,
+          services_carte,
+          category,
+          properties,
+        })
+        res.json(logementDoc);
+    }
+    
+    if (method === 'PUT') {
+      const {
+        name_resident,
+        description,
+        type_apartment,
+        price_origin_apartment,
+        price_promo_apartment,
+        adress,
+        images,
+        info_apartment,
+        tels,
+        advantages,
+        services_include,
+        services_carte,
+        category,
+        properties,
+      } = req.body;
+        await Logement.updateOne({_id}, {name_resident,
+          description,
+          type_apartment,
+          price_origin_apartment,
+          price_promo_apartment,
+          adress,
+          images,
+          info_apartment,
+          tels,
+          advantages,
+          services_include,
+          services_carte,
+          category,
+          properties});
+        res.json(true);
+      }
+
+    if (method === "DELETE") {
+        if (req.query?.id) {
+            await Logement.deleteOne({_id: req.query?.id});
+            res.json(true);
+        }
+    }
+    
+}
